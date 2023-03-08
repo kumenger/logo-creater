b49234003609875977e3967aa1d0131eae0e2d2f
kumenger <44558777+kumenger@users.noreply.github.com>
2023-03-07 19:10:05 -0700

    intial test added

--- test/logo.test.js
+++ test/logo.test.js
@@ -6,9 +6,14 @@ describe('Create Logo', () => {
   // This test checks to see if 2 % 2 has a remainder of 0 and returns 0.
   describe('Create tringle Shape logo', () => {
     it('return logo shaped tringle', () => {
-      const total = 0;
-      const arithmetic = new Arithmetic();
-      expect(arithmetic.modulus(2, 2)).toEqual(total);
+      const renderLogo = `<svg width="500" height="500" version="1.1" xmlns="http://www.w3.org/2000/svg" >
+
+       
+      <polygon   points="250,60 100,400 400,400" width="60" height="60" stroke="black" fill="red" stroke-width="1"/>
+      <text text-anchor="middle"  font-size="15px"  x="250" y="200" fill="red">kume</text>
+   </svg>`;
+      const newLogo = new CreateLogo.Trinagle({ text: 'kume', ShapeColor: 'red', textColor: 'red', shape: 'Circle' });
+      expect(newLogo.render()).toBe(renderLogo);
     });
   });
 });
